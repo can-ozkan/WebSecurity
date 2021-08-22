e7a764e8e6105913a6da17b4da9ab04ac32aaeee
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2021-08-22 21:19:54 +0300

    Update company_db.sql

--- sql_course/company_db.sql
+++ sql_course/company_db.sql
@@ -115,3 +115,12 @@ INSERT INTO works_with VALUES(105, 404, 33000);
 INSERT INTO works_with VALUES(107, 405, 26000);
 INSERT INTO works_with VALUES(102, 406, 15000);
 INSERT INTO works_with VALUES(105, 406, 130000);
+
+SELECT * FROM employee;
+SELECT * FROM client;
+SELECT * FROM employee ORDER BY salary DESC;
+SELECT * FROM employee ORDER BY sex, first_name, last_name;
+SELECT * FROM employee LIMIT 5;
+SELECT first_name, last_name FROM employee;
+SELECT first_name AS forename, last_name AS surname FROM employee;
+SELECT DISTINCT sex FROM employee;
