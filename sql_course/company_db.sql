7315aa2d68bf77044a3e3671b86ad0098262d38a
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2021-08-23 20:54:34 +0300

    Update company_db.sql

--- sql_course/company_db.sql
+++ sql_course/company_db.sql
@@ -192,5 +192,14 @@ WHERE employee.emp_id in (
   WHERE works_with.total_sales > 30000
 );
 
+--Find all clients who are handled by the branch that Michael Scott manages. Assume you know Michael's ID
+SELECT client.client_name
+FROM client
+WHERE client.branch_id = (
+  SELECT branch.branch_id
+  FROM branch
+  WHERE branch.mgr_id = 102
+);
+
 
 
